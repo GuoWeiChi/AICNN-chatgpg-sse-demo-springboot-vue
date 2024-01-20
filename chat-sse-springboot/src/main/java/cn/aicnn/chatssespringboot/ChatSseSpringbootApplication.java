78086c379d6f1125e8775e32f7de1e675946cc80
SCJT <SCJT>
2024-01-20 23:20:08 +0800

    init project

---
+++ chat-sse-springboot/src/main/java/cn/aicnn/chatssespringboot/ChatSseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.aicnn.chatssespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSseSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatSseSpringbootApplication.class, args);
+    }
+
+}
---
+++ chat-sse-springboot/src/test/java/cn/aicnn/chatssespringboot/ChatSseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.aicnn.chatssespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSseSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
